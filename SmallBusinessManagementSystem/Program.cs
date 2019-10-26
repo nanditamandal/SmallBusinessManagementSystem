955f496209fbd7e0a3eaf59b2f1e291c3dc54fa4
nanditamandal <nanditamandal.19@gmail.com>
2019-10-26 17:35:20 +0600

    customer , product

---
+++ SmallBusinessManagementSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmallBusinessManagementSystem.Model;
+using SmallBusinessManagementSystem.UI;
+
+namespace SmallBusinessManagementSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new SalesUI());
+            //Application.Run(new CategoryUI());
+            //Application.Run(new ProductUI());
+            Application.Run(new CustomerUI());
+        }
+    }
+}
